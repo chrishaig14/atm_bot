253b5a3facd4c2620dc9a444df3088b5fc232098
chris <chrishaig14@gmail.com>
2019-10-24 21:55:55 -0300

    Add ugly message & command parsing

--- main.py
+++ main.py
@@ -18,3 +18,20 @@ while True:
         print('message: ', msg)
         requests.post(bot_url + "/sendMessage", {'chat_id': msg['message']['chat']['id'],
                                                  'text': 'You said \'{}\''.format(msg['message']['text'])})
+        if 'entities' not in msg['message']:
+            requests.post(bot_url + "/sendMessage", {'chat_id': msg['message']['chat']['id'],
+                                                     'text': 'Sorry, just send a command /link, /banelco'})
+        else:
+            entities = msg['message']['entities']
+            if len(entities) != 1:
+                requests.post(bot_url + "/sendMessage", {'chat_id': msg['message']['chat']['id'],
+                                                         'text': 'More than one entity: Sorry, just send a command /link, /banelco'})
+            else:
+                entity = entities[0]
+                if entity['type'] != "bot_command":
+                    requests.post(bot_url + "/sendMessage", {'chat_id': msg['message']['chat']['id'],
+                                                             'text': 'Not a command: Sorry, just send a command /link, /banelco'})
+                else:
+                    command = msg['message']['text'][entity['offset']:entity['offset'] + entity['length']]
+                    requests.post(bot_url + "/sendMessage", {'chat_id': msg['message']['chat']['id'],
+                                                             'text': 'Received command' + command})
