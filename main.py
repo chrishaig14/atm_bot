a855acad470519677d5ae657e57ca66cea377053
chrishaig14 <chrishaig14@gmail.com>
2019-10-26 21:48:13 -0300

    Search & add nearest places to map (not finished)

--- main.py
+++ main.py
@@ -10,16 +10,64 @@ last_update = 0
 command_history = {}
 api_key = '7sopi7Ekw99TV5rYxGXrzXIkq9dOZTAL'
 
+###########
+
+import pandas as pd
+
+from kdtree import *
+
+data = pd.read_csv("cajeros-automaticos.csv")
+
+xmin = data['long'].min()
+xmax = data['long'].max()
+ymin = data['lat'].min()
+ymax = data['lat'].max()
+
+print(xmin)
+
+banelco = data[data['red'] == 'BANELCO']
+link = data[data['red'] == 'LINK']
+
+p_link = link[['lat', 'long']].to_numpy()
+
+plt.scatter(p_link[:, 0], p_link[:, 1])
+plt.show()
+
+kd_link = make_kdtree(p_link, axis=0)
+plot_points(p_link)
+num = 3
+q = [(xmax + xmin) / 2, (ymax + ymin) / 2]
+b = search_closest_kdtree(kd_link, q, num)
+b = np.array(b)
+
+
+# plt.scatter(b[:, 0], b[:, 1], color='b')
+# plt.scatter(q[0], q[1], color='g')
+# plt.show()
+##################
+
+def make_map_url(start, locations):
+    start = '{},{}|marker-start'.format(start[0], start[1])
+    ls = ""
+    for i, l in enumerate(locations):
+        ls += "{},{}|marker-{}||".format(l[0], l[1], i + 1)
+
+    return 'https://www.mapquestapi.com/staticmap/v5/map?locations={}||{}&zoom=16&size=600,400@2x&key={}'.format(start,
+                                                                                                                 ls,
+                                                                                                                 api_key)
+
 
 def handle_msg(msg):
     print('message: ', msg)
     if 'location' in msg:
         location = msg['location']
-        locations = '{},{}|marker-start|'.format(location['latitude'], location['longitude'])
+        start = [location['latitude'], location['longitude']]
+        locations = search_closest_kdtree(kd_link, start, 5)
+        map_url = make_map_url(start, locations)
+        print("MAP URL: ", map_url)
         x = requests.post(bot_url + "/sendPhoto", {'chat_id': msg['chat']['id'],
-                                               'photo': 'https://www.mapquestapi.com/staticmap/v5/map?locations={}&zoom=16&size=600,400@2x&key={}'.format(
-                                                   locations,
-                                                   api_key), 'reply_markup': json.dumps({'remove_keyboard': True})})
+                                                   'photo': map_url,
+                                                   'reply_markup': json.dumps({'remove_keyboard': True})})
         print("response: ", x.json())
     if 'text' in msg:
         requests.post(bot_url + "/sendMessage", {'chat_id': msg['chat']['id'],
@@ -32,11 +80,12 @@ def handle_msg(msg):
                     command = msg['text'][entity['offset']:entity['offset'] + entity['length']]
                     if command == "/link" or command == "/banelco":
                         ok = True
-                        x = requests.post(bot_url + "/sendMessage",
-                                          {'chat_id': msg['chat']['id'], 'text': "Please send your location",
-                                           'reply_markup': json.dumps({
-                                               'keyboard': [[{'text': 'Send location', "request_location": True}]],
-                                               "one_time_keyboard": True, "resize_keyboard": True})})
+                        text = "Please send your location"
+                        reply_markup = json.dumps({
+                            'keyboard': [[{'text': 'Send location', "request_location": True}]],
+                            "one_time_keyboard": True, "resize_keyboard": True})
+                        msg_obj = {'chat_id': msg['chat']['id'], 'text': text, 'reply_markup': reply_markup}
+                        x = requests.post(bot_url + "/sendMessage", msg_obj)
         if not ok:
             requests.post(bot_url + "/sendMessage", {'chat_id': msg['chat']['id'],
                                                      'text': 'Sorry, just send a command /link, /banelco'})
