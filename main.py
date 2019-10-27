8a0ec6e10820fda6ef3f6455fa71eb1c2809554a
chrishaig14 <chrishaig14@gmail.com>
2019-10-26 21:23:28 -0300

    Simplify message handling

--- main.py
+++ main.py
@@ -15,45 +15,31 @@ def handle_msg(msg):
     print('message: ', msg)
     if 'location' in msg:
         location = msg['location']
-        x = requests.post(bot_url + "/sendMessage", {'chat_id': msg['chat']['id'],
-                                                     'text': 'Your location is \'{},{}\''.format(
-                                                         location['latitude'],
-                                                         location['latitude']),
-                                                     'reply_markup': json.dumps({'remove_keyboard': True})})
         locations = '{},{}|marker-start|'.format(location['latitude'], location['longitude'])
-        requests.post(bot_url + "/sendPhoto", {'chat_id': msg['chat']['id'],
+        x = requests.post(bot_url + "/sendPhoto", {'chat_id': msg['chat']['id'],
                                                'photo': 'https://www.mapquestapi.com/staticmap/v5/map?locations={}&zoom=16&size=600,400@2x&key={}'.format(
                                                    locations,
-                                                   api_key)})
+                                                   api_key), 'reply_markup': json.dumps({'remove_keyboard': True})})
         print("response: ", x.json())
     if 'text' in msg:
         requests.post(bot_url + "/sendMessage", {'chat_id': msg['chat']['id'],
                                                  'text': 'You said \'{}\''.format(msg['text'])})
-        if 'entities' not in msg:
+        ok = False
+        if 'entities' in msg:
+            if len(msg['entities']) == 1:
+                entity = msg['entities'][0]
+                if entity['type'] == 'bot_command':
+                    command = msg['text'][entity['offset']:entity['offset'] + entity['length']]
+                    if command == "/link" or command == "/banelco":
+                        ok = True
+                        x = requests.post(bot_url + "/sendMessage",
+                                          {'chat_id': msg['chat']['id'], 'text': "Please send your location",
+                                           'reply_markup': json.dumps({
+                                               'keyboard': [[{'text': 'Send location', "request_location": True}]],
+                                               "one_time_keyboard": True, "resize_keyboard": True})})
+        if not ok:
             requests.post(bot_url + "/sendMessage", {'chat_id': msg['chat']['id'],
                                                      'text': 'Sorry, just send a command /link, /banelco'})
-        else:
-            entities = msg['entities']
-            if len(entities) != 1:
-                requests.post(bot_url + "/sendMessage", {'chat_id': msg['chat']['id'],
-                                                         'text': 'More than one entity: Sorry, just send a command /link, /banelco'})
-            else:
-                entity = entities[0]
-                if entity['type'] != "bot_command":
-                    requests.post(bot_url + "/sendMessage", {'chat_id': msg['chat']['id'],
-                                                             'text': 'Not a command: Sorry, just send a command /link, /banelco'})
-                else:
-                    command = msg['text'][entity['offset']:entity['offset'] + entity['length']]
-                    # requests.post(bot_url + "/sendMessage", {'chat_id': msg['chat']['id'],
-                    #                                          'text': 'Received command' + command})
-                    # requests.post(bot_url + "/sendPhoto", {'chat_id': msg['chat']['id'],
-                    #                                        'photo': 'https://source.unsplash.com/random/600x600'})
-                    x = requests.post(bot_url + "/sendMessage",
-                                      {'chat_id': msg['chat']['id'], 'text': "Please send your location",
-                                       'reply_markup': json.dumps({
-                                           'keyboard': [[{'text': 'Send location', "request_location": True}]],
-                                           "one_time_keyboard": True, "resize_keyboard": True})})
-                    print("result: ", x.json())
 
 
 while True:
