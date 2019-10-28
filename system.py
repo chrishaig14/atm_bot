66f6ecf37798eb7caed7db8d617832f361a2359a
chrishaig14 <chrishaig14@gmail.com>
2019-10-28 14:52:14 -0300

    Add thread to reset available uses at 8am mon-fri

--- system.py
+++ system.py
@@ -1,12 +1,35 @@
 import pandas as pd
 from kdtree import *
+import threading
+import time
 
 PI = 3.14159265
 R = 6371000  # Earth radius in meters
-
+N = 10
 
 class System:
+    def reset_uses(self):
+        updated = False # this is to prevent updating more than once at 08:xx
+        while True:
+            now = time.localtime()
+            if now.tm_hour == 8 and now.tm_wday < 5 and not updated:
+                self.lock.acquire()
+                print("#################### RESETTING USES at ", now)
+                self.uses = {id:N for id in self.points_x_y['id']}
+                for p in self.map_id_to_point.values():
+                    p.active= True
+                self.lock.release()
+                print(self.uses)
+                updated = True
+            else:
+                updated = False
+            time.sleep(60)
+
+
     def __init__(self):
+        self.lock = threading.Lock()
+        reset_thread = threading.Thread(target=self.reset_uses)
+
         self.points_long_lat = pd.read_csv("cajeros-automaticos.csv")
         self.points_long_lat_rad = self.points_long_lat.copy() # deep copy to keep original intact
         self.points_long_lat_rad[['long','lat']]=self.points_long_lat_rad[['long','lat']]*PI/180
@@ -16,12 +39,13 @@ class System:
         self.points_x_y = self.points_long_lat_rad.copy()
         self.points_x_y['long'] = R * self.points_x_y['long'] * np.cos(self.l0)
         self.points_x_y['lat'] = R * self.points_x_y['lat']
-        self.uses = {id:1000 for id in self.points_x_y['id']}
+        self.uses = {id:N for id in self.points_x_y['id']}
         #print("USES:" ,self.uses)
         #print(self.points_x_y)
 
 
         self.map_id_to_point = {p['id']:Point(p['id'],[p['long'],p['lat']],True) for i,p in self.points_x_y.iterrows()}
+        reset_thread.start()
 
         link_points = [self.map_id_to_point[p['id']] for i,p in self.points_x_y.loc[self.points_x_y['red'] == "LINK"].iterrows()]
         banelco_points = [self.map_id_to_point[p['id']] for i,p in self.points_x_y.loc[self.points_x_y['red'] == "BANELCO"].iterrows()]
@@ -45,6 +69,9 @@ class System:
         q = [long * PI / 180, lat * PI / 180]
         q[0] = R * q[0] * np.cos(self.l0)
         q[1] = R * q[1]
+
+        self.lock.acquire()
+
         results = search_closest_kdtree(kd_search, q, 3)
         l = []
         # keep only the places which are within 500m
@@ -57,6 +84,11 @@ class System:
         ids = [p['point'].ID for p in results]
         r=pd.DataFrame({'id':ids}).merge(self.points_long_lat)
         self.simulate_use(ids)
+
+
+        self.lock.release()
+
+
         #print(self.uses)
         return r,{p['point'].ID:p['distance'] for p in results}
     def simulate_use(self,r):
